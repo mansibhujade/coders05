ff8f1c896200a60a829212af3958641a2cd2f9c1
ishant-rambhad <118370814+ishant-rambhad@users.noreply.github.com>
2022-11-20 22:35:53 +0530

    Update add books.py

--- add books.py
+++ add books.py
@@ -18,13 +18,17 @@ if __name__ =="__main__":
         place(height=50,width=100,x=50,y=150)
     book_name_text = Entry(gui).place(height=30, width=200, x=200, y=150)
 
-    book_rs=Label(gui,text="BOOK RS").\
+    title_name=Label(gui,text="TITLE").\
         place(height=50,width=100,x=50,y=200)
-    book_rs_text = Entry(gui).place(height=30, width=200, x=200, y=200)
+    title_name_text = Entry(gui).place(height=30, width=200, x=200, y=200)
 
     author_name=Label(gui,text="AUTHOR NAME").\
         place(height=50,width=100,x=50,y=250)
     book_name_text = Entry(gui).place(height=30, width=200, x=200, y=250)
+    
+    copies=Label(gui,text="COPIES").\
+        place(height=50,width=100,x=50,y=300)
+    copies_text = Entry(gui).place(height=30, width=200, x=200, y=300)
 
     submit_button = Button(gui, text="Submit").\
         place(height=50, width=100, x=500, y=450)
